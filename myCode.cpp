5c35d1be4382d2df2999ff1180ac8da89b1feaae
mean <fixounet@free.fr>
2019-01-20 15:56:57 +0100

    [freertos] Add as submodule

--- myCode.cpp
+++ myCode.cpp
@@ -31,7 +31,12 @@ void mySetup()
     Serial.println("Init"); 
     
     identifier = tft->readID();
-    tft=Adafruit_TFTLCD_8bit_STM32::spawn(identifier);    
+    if(!identifier) identifier=0x9341;
+    tft=Adafruit_TFTLCD_8bit_STM32::spawn(0x7789);   
+    if(!tft)
+    {
+        while(1) {};
+    }
     tft->begin();
     tft->setRotation(1);
     tft->setFontFamily(&Targa56pt7b, &DIGIT_LCD56pt7b, &DIGIT_LCD56pt7b);
@@ -42,10 +47,15 @@ static uint32_t gpioA[10];
  * 
  */
 void myLoop(void) 
-{    
+{  
+
     uint32_t start,end,org,mine;
+#if 0    
     static int rotation=1;
     tft->setRotation(rotation);
+    
+    static int offset=0;
+    offset=(offset+1)%15;
     
 #if 0
     tft->setTextSize(0);
@@ -68,10 +78,10 @@ void myLoop(void)
          
      
     tft->setTextColor(RED,BLUE);
-    tft->setCursor(10,140);
+    tft->setCursor(10+offset,140);
     tft->setFontSize(Adafruit_TFTLCD_8bit_STM32::SmallFont);
     start=millis();     
-    tft->myDrawString("Hello");
+    tft->myDrawString("Hello",320-30);
     end=millis();
     
     tft->setTextSize(5);
@@ -85,6 +95,51 @@ void myLoop(void)
     
     tft->println(end-start);
     //delay(5000);
+#endif
+#if 0
+    
+#define ENC_BUTTON    0
+#define VDIV_BUTTON   1
+#define SECDIV_BUTTON 2
+#define TRIG_BUTTON   3
+#define OK_BUTTON     4
+
+
+extern t_config config;
+extern uint8_t currentFocus;
+extern volatile bool hold;
+extern volatile bool keepSampling;
+extern volatile bool holdSampling;
+
+volatile uint32_t pressedTime[5] = {0,0,0,0,0};
+uint16_t pin[5] = {DB3_Pin,DB4_Pin,DB5_Pin,DB6_Pin,DB7_Pin};
+GPIO_TypeDef* port[5] ={DB3_GPIO_Port,DB4_GPIO_Port,DB5_GPIO_Port,DB6_GPIO_Port,DB7_GPIO_Port};
+    
+#endif
+    tft->setTextSize(3);
+    start=millis();
+    tft->fillScreen(BLACK);
+    end=millis();
+    tft->setCursor(200, 70);    
+    tft->println(end-start);
+    
+    // Read GPIOB
+    uint32_t val= GPIOB->regs->IDR;
+    tft->setCursor(200, 90);    
+#define CHECK_BUTTON(pin, txt) if(!(val & 1<<pin)) tft->println(txt);
+    
+    CHECK_BUTTON(3,"B ");
+    CHECK_BUTTON(7,"Ok ");
+    CHECK_BUTTON(6,"Tr ");
+    CHECK_BUTTON(5,"Se ");
+    CHECK_BUTTON(4,"Vt ");
+    
+    
+    
+    delay(200);
+    
+      
+     
 }
 
 
