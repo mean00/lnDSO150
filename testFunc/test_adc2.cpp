1a8b41a0f2c254b93f6cb5d564ef692c9c99209c
mean <fixounet@free.fr>
2019-02-12 07:56:40 +0100

    [adc/test] slightly prettuer test screen

--- testFunc/test_adc2.cpp
+++ testFunc/test_adc2.cpp
@@ -18,6 +18,8 @@
 #include "HardwareSerial.h"
 #include "dso_adc.h"
 extern void splash(void);
+
+void drawGrid(void);
 //--
 extern Adafruit_TFTLCD_8bit_STM32 *tft;
 extern DSOControl *controlButtons;
@@ -29,44 +31,69 @@ extern DSOADC    *adc;
 void testAdc2(void)
 {
     int reCounter=0;
-    controlButtons->setInputGain(7); // x1.4
     
-    uint16_t samples[256];
+    
+    int samples[256];
     
     int currentScale=10;
+    
+    // Calibrate...
+    
+    int cal=0;
+    int avg;
+    controlButtons->setInputGain(0);
+    for(int i=0;i<256;i++)
+    {
+        cal+=analogRead(PA0);
+        xDelay(2);
+    }
+    cal/=256;
+    
+    controlButtons->setInputGain(7); // x1.4
     adc->setTimeScale(currentScale); // 10 us *1024 => 10 ms scan
     while(1)
     {
         splash();
+        drawGrid();
         tft->setCursor(200, 160);
         tft->print(currentScale);
-
+        
       //  for(int i=0;i<16;i++)
         {
             int count;
             int i=0;
-            adc->takeSamples(256);
+            adc->takeSamples(240);
             uint32_t *xsamples=adc->getSamples(count);
             for(int j=0;j<count;j++)
             {
-                samples[j]=xsamples[j] >>16; //&0xffff;
+                samples[j]=(int)(xsamples[j] >>16)-cal; //&0xffff;
             }
 
-            int min=4095,max=0;
+            int min=4095,max=-4096;
+            avg=0;
             for(int j=0;j<count;j++)
             {
                 if(samples[j]<min) min=samples[j];
                 if(samples[j]>max) max=samples[j];
+                avg+=samples[j];
 
             }
+            avg/=count;
                         
-            for(int j=0;j<count;j++)
+            float last=samples[0]; // 00--4096
+            last/=20;
+            for(int j=1;j<count;j++)
             {
-                float f=samples[j]; // 00--4096
-                f/=20; // 0..200
-                tft->drawPixel(16*i+j,240-f,YELLOW);
+                float next=samples[j]; // 00--4096
+                next/=20; // 0..200
+                if(next>239) next=239;
+                if(next<0) next=0;
+                tft->drawFastVLine(16*i+j,last,next,YELLOW);
+                last=next;
             }
         }
+        tft->setCursor(200, 200);
+        tft->print((int)avg);
         xDelay(300);
         int inc=controlButtons->getRotaryValue();
         if(inc)
@@ -83,3 +110,15 @@ void testAdc2(void)
     }
 }
 //-
+#define SCALE_STEP 24
+#define C 10
+void drawGrid(void)
+{
+    uint16_t fgColor=(0x1F)<<5;
+    for(int i=0;i<=C;i++)
+    {
+        tft->drawFastHLine(0,SCALE_STEP*i,SCALE_STEP*C,fgColor);
+        tft->drawFastVLine(SCALE_STEP*i,0,SCALE_STEP*C,fgColor);
+    }
+       tft->drawFastHLine(0,239,SCALE_STEP*C,fgColor);
+}
