c82e926dc8e5faeeadf752050809f164dd055129
mean <fixounet@free.fr>
2019-06-22 17:05:27 +0200

    [dso] split menu and menuEngine

--- dso_menu.cpp
+++ dso_menu.cpp
@@ -5,7 +5,7 @@
  ****************************************************/
 
 #include "dso_includes.h"
-#include "dso_menu.h"
+#include "dso_menuEngine.h"
 #include "dso_global.h"
 #include "dso_calibrate.h"
 extern testSignal *myTestSignal;
@@ -14,8 +14,6 @@ void updateFrequency(int fq)
     myTestSignal->setFrequency(fq);
 }
 #define MKFQ(x,y) void fq##x() {updateFrequency(y); }
-#define USE_MENU_BUTTON DSOControl::DSO_BUTTON_ROTARY
-//#define USE_MENU_BUTTON DSOControl::DSO_BUTTON_OK
 
 MKFQ(100,100)
 MKFQ(1000,1000)
@@ -60,144 +58,4 @@ void  menuManagement(void)
      MenuManager man(tem);
      man.run();
 }
-/**
- * 
- * @param menu
- */
-MenuManager::MenuManager(const MenuItem *menu)
-{
-    _menu=menu;
-}
-/**
- * 
- */
-MenuManager::~MenuManager()
-{
-    _menu=NULL;
-}
-/**
- * 
- * @param onoff
- * @param line
- * @param text
- * @return 
- */
-void MenuManager::printMenuEntry(bool onoff, int line,const char *text)
-{
-    #define BG_COLOR GREEN    
-    tft->setCursor(100,40+24*line);
-    if(onoff)
-        tft->setTextColor(BLACK,BG_COLOR); 
-    else  
-        tft->setTextColor(BG_COLOR,BLACK);
-    tft->myDrawString(text);
-}
-void MenuManager::printMenuTitle(const char *text)
-{
-    tft->setTextColor(GREEN,BLACK); 
-    tft->setCursor(80,40-24);
-    tft->myDrawString(text);
-}
-/**
- * 
- */
-void MenuManager::run(void)
-{
-     tft->fillScreen(BLACK);
-     tft->setFontSize(Adafruit_TFTLCD_8bit_STM32::BigFont);
-     runOne(_menu);
-
-};
-/**
- */
-void MenuManager::redraw(const char *title, int n,const MenuItem *xtop, int current)
-{
-    tft->fillScreen(BLACK);
-    printMenuTitle(title); 
-    for(int i=0;i<n;i++)
-    {
-        printMenuEntry(current==i,i,xtop[i].menuText);
-    }        
-}
-
-void MenuManager::blink(int current, const char *text)
-{
-    for(int i=0;i<5;i++)
-    {
-           printMenuEntry(false,current,text);
-           xDelay(80);
-           printMenuEntry(true,current,text);
-           xDelay(80);
-    }     
-}
-
-/**
- */
-void MenuManager::runOne( const MenuItem *xtop)
-{
-     tft->fillScreen(BLACK);
-     const char *title=xtop->menuText;
-     xtop=xtop+1;
-     int n=0;
-     {
-        const MenuItem  *top=xtop;
-        while(top->type!=MenuItem::MENU_END)
-        {
-            top++;
-            n++;
-        }
-     }
-     
-     
-     
-     // draw them 
-     // 0 to N-1
-     int current=0;
-     while(1)
-     {
-next:         
-        redraw(title,n,xtop,current);
-        while(1)
-        {
-                  if(controlButtons->getButtonEvents(USE_MENU_BUTTON) & EVENT_LONG_PRESS)
-                    return;
-                  if(controlButtons->getButtonEvents(USE_MENU_BUTTON) & EVENT_SHORT_PRESS)
-                  {
-                      switch(xtop[current].type)
-                      {
-                      case MenuItem::MENU_BACK: return; break;
-                      case MenuItem::MENU_KEY_VALUE:  break;
-                      case MenuItem::MENU_SUBMENU: 
-                            {
-                                const MenuItem *sub=(const MenuItem *)xtop[current].cookie;
-                                runOne(sub);
-                                goto next;
-                            }
-                            break;
-                      case MenuItem::MENU_CALL: 
-                            {
-                                typedef void cb(void);
-                                cb *c=(cb *)xtop[current].cookie;
-                                blink(current,xtop[current].menuText);
-                                c();
-                                goto next;
-                            }
-                          break;
-                      case MenuItem::MENU_END: return;break;
-                      case MenuItem::MENU_TOGGLE: break;
-                      default: xAssert(0);break;
-                      }
-                      break;
-                  }
-                  int inc=controlButtons->getRotaryValue();
-                  if(inc)
-                  {
-                    current+=inc;
-                    while(current<0) current+=n;
-                    while(current>=n) current-=n;
-                    break;
-                  }
-        }
-     }
-};
 // EOF
