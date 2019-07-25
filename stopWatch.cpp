f0a0a1b43b6cf1e74d5175b21a37e0c6cd6a0225
mean <fixounet@free.fr>
2019-07-25 07:59:50 +0200

    [stopWatch] Fix case where newTime==oldTime, that should fix the fake assert

--- stopWatch.cpp
+++ stopWatch.cpp
@@ -3,8 +3,9 @@
 #include "dso_global.h"
 #include "stopWatch.h"
 #pragma once
-
+static void tag();
 /**
+ * return true if the time since the last ok() is greater than threshold
  */
 bool StopWatch::elapsed(int threshold)
 {
@@ -14,15 +15,29 @@ bool StopWatch::elapsed(int threshold)
         mil=millis()&0xffff;
         return false;
     }
-    newMil=millis();
-    if(mil<newMil)
+    newMil=millis()&0xffff;
+    int delta;
+    if(mil<=newMil)
     {
-        return threshold<(newMil-mil);
+        delta=newMil-mil;        
+    }else
+    {
+        delta=(0x10000+mil)-newMil;
     }
-    return threshold<(0x10000+mil-newMil);
+    if (threshold<delta)
+    {
+      //  tag();
+        return true;
+    }
+    return false;
 }
 
 void StopWatch::ok()
 {
-    mil=millis();
+    mil=millis()&0xffff;
+}
+
+static void tag()
+{
+    xAssert(0);
 }
