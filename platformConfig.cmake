4a82da2e65b1f75788f4480f36b12efb35e8a262
mean <fixounet@free.fr>
2020-01-31 08:29:56 +0100

    DSO: simpler build setup

---
+++ platformConfig.cmake
@@ -0,0 +1,11 @@
+SET(ARDUINO_USE_NEWLIB 1)
+SET(PLATFORM_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Arduino_STM32/STM32F1/")
+
+# Warning the paths are mingw i.e. c:\dev becomes /c/dev
+IF(WIN32)
+    SET(ARDUINO_SDK_PATH "/c/dev/Arduino")
+    SET(PLATFORM_TOOLCHAIN_PATH  "/c/dev/arm83/bin")
+ELSE(WIN32)
+    # ARDUINO_SDK_PATH should be autodetected
+    SET(PLATFORM_TOOLCHAIN_PATH "/home/fx/Arduino_stm32/arm-none-eabi-gcc/download/gcc-arm-none-eabi-8.2.1-1.7/bin")
+ENDIF(WIN32)
