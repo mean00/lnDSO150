5ac09b4d24ca81466d37bd34b6e03d7efe5b3cc9
mean <fixounet@free.fr>
2020-05-05 08:05:29 +0200

    cleanup

---
+++ testFunc/testFunc.h
@@ -0,0 +1,14 @@
+#pragma once
+extern void testTestSignal();
+extern void testButtons();
+extern void testAdc();
+extern void testAdc2();
+extern void testAdc3();
+extern void testDisplay();
+extern void testCalibrate(void);
+extern void testI2c( void);
+extern void testCapture(void);
+extern void testTrigger(void);
+extern void testAdcWatchdog(void);
+extern void testButtonCoupling(void);
+extern void testCalibrate(void);
