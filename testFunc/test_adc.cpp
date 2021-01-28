c6ed9d62395a7a1d7316660ccf60d395aea731bf
mean <fixounet@free.fr>
2021-01-28 08:33:21 +0100

    simple test for directADCxread

--- testFunc/test_adc.cpp
+++ testFunc/test_adc.cpp
@@ -6,6 +6,8 @@
 
 #include "dso_includes.h"
 #include "dso_test_signal.h"
+#include "simpleADC.h"
+#include "pinConfiguration.h"
 extern void splash(void);
 
 static void drawGrid(void);
@@ -75,4 +77,68 @@ void drawGrid(void)
         tft->drawFastVLine(SCALE_STEP*i,0,SCALE_STEP*C,fgColor);
     }
        tft->drawFastHLine(0,239,SCALE_STEP*C,fgColor);
+}
+extern uint16_t directADC1Read(int pin);
+extern uint16_t directADC2Read(int pin);
+
+
+// -- simpleAdc xdc(COUPLING_PIN)-> works
+// directADC1Read-> KO
+// directADC2Read-> KO
+
+void testCoupling()
+{ 
+    
+    extern void Logger(const char *fmt...);
+    extern uint16_t directADC2Read(int pin);
+#if 0    
+    while(1)
+    {
+          pinMode(COUPLING_PIN,INPUT_ANALOG);
+          int rawCoupling= analogRead(COUPLING_PIN);
+          Logger("CPL=%d\n",rawCoupling);
+          xDelay(300);
+    }
+#else
+    
+     pinMode(COUPLING_PIN,INPUT_ANALOG);
+     
+     //simpleAdc xdc(COUPLING_PIN);
+     
+    while(1)
+    {
+        uint16_t *data;
+        int rawCoupling=0;
+
+#if 0        
+        int nb=4;
+        xdc.sample(nb,&data,ADC_SMPR_13_5,DSOADC::ADC_PRESCALER_4);
+        for(int i=0;i<nb;i++)
+            rawCoupling+=data[i];
+        rawCoupling/=nb;
+#else
+        rawCoupling=directADC2Read(COUPLING_PIN);
+#if 0        
+        //--
+        int length=1;
+        adc_reg_map *regs = ADC2->regs;
+        uint32 tmp = regs->SQR1;
+        tmp &= ~ADC_SQR1_L;
+        tmp |= (length - 1) << 20;
+        regs->SQR1 = tmp;
+
+        regs->SQR3 = 5;
+        regs->CR2 |= ADC_CR2_SWSTART;
+        while (!(regs->SR & ADC_SR_EOC))
+        ;
+
+        rawCoupling= (uint16)(regs->DR & ADC_DR_DATA);
+#endif        
+        
+#endif        
+        Logger("CPL=%d\n",rawCoupling);
+        xDelay(300);
+    }
+     
+#endif    
 }
