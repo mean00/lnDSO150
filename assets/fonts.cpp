bf918c9840df501e2dba240c004b3a1a5ad9d991
mean <fixounet@free.fr>
2021-10-09 08:26:16 +0200

    change font, switch to opensans

--- assets/fonts.cpp
+++ assets/fonts.cpp
@@ -1,17 +1,18 @@
 #include "lnArduino.h"
 #include "gfxfont.h"
 #include "Fonts/waree9.h"
-#include "Fonts/waree12.h"
-
+//#include "Fonts/waree12.h"
+#include "Fonts/OpenSans_Regular14pt7b.h"
+//#include "Fonts/ostrich_regular14pt7b.h"
 const GFXfont *smallFont()
 {
     return &Waree9pt7b;
 }
 const GFXfont *mediumFont()
 {
-    return &Waree12pt7b;
+    return &OpenSans_Regular14pt7b;
 }
 const GFXfont *bigFont()
 {
-    return &Waree12pt7b;
+    return mediumFont();
 }
