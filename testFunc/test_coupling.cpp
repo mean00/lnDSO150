90b5f76eaa5c0e28ab3971de11d483ace05204a4
mean <fixounet@free.fr>
2021-10-17 17:33:16 +0200

    update test for coupling

--- testFunc/test_coupling.cpp
+++ testFunc/test_coupling.cpp
@@ -5,15 +5,14 @@
 #include "lnStopWatch.h"
 
 extern DSOControl * control;
-
 void testCoupling()
 {
     DSO_GFX::clear(0);
     while(1)
     {
-        const char *t=control->geCouplingStateAsText();
-        DSO_GFX::printxy(1,5,t);
-        Logger("Coupling %s\n",t);
+        const char *p=control->geCouplingStateAsText();
+        DSO_GFX::printxy(3,3,p);
+        Logger("%s\n",p);
         xDelay(200);
     }
     
