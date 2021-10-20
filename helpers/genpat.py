b34a8e1951f5f92d08a37fe8ef167776078ea627
mean <fixounet@free.fr>
2021-10-20 07:07:08 +0200

    update pix/div to 20

--- helpers/genpat.py
+++ helpers/genpat.py
@@ -1,13 +1,13 @@
 #
 import array as arr
 #
-width=192
+width=200
 def genArray(ar,tx):
     out="const uint16_t "+tx+"["+str(width)+"]={\n"
     for i in range(0,width):
         out+=str(ar[i])
         out+=str(",")
-        if((i % 24)==23):
+        if((i % 20)==19):
             out+="\n"
     out+="};\n"
     print(out)
@@ -16,18 +16,19 @@ patdef = arr.array('i')
 patlightgreen = arr.array('i')
 patdarkgreen = arr.array('i')
 #
-darkgreen=0xf<<5 # Dark green
+darkgreen=0x1f<<5 # Dark green
 lightgreen=0x2f<<5 # Light green
 for i in range(0,width):
     patdef.append(0)
     patlightgreen.append(lightgreen)
     patdarkgreen.append(darkgreen)
-    if( (i/24)*24==i):
+    if( (i%20)==0):
         patdef[i]=darkgreen
 #
 patdef[0]=          lightgreen
 patdef[width-1]=    lightgreen
-patdef[1+width/2] = lightgreen
+midpoint=int(1+width/2)
+patdef[midpoint] = lightgreen
 
 genArray(patdef,"defaultPattern")
 genArray(patlightgreen,"lightGreenPattern")
