192e2f63885787b9d39d32197ab46ca31554346c
mean <fixounet@free.fr>
2021-12-11 08:48:39 +0100

    switch to picolibc

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -5,19 +5,24 @@ IF(1) # ARM
         SET(LN_MCU             "M3"  CACHE INTERNAL "")
         SET(LN_MCU_FLASH_SIZE  128   CACHE INTERNAL "")
         SET(LN_MCU_RAM_SIZE    20    CACHE INTERNAL "")
+        SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
     ELSE()
         SET(LN_ARCH            "ARM" CACHE INTERNAL "")
         SET(LN_MCU             "M4"  CACHE INTERNAL "")
         SET(LN_MCU_FLASH_SIZE  256   CACHE INTERNAL "")
         SET(LN_MCU_RAM_SIZE    20    CACHE INTERNAL "")
         SET(LN_MCU_SPEED 72000000    CACHE INTERNAL "") #=> ok
+        SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
         #SET(LN_MCU_SPEED 104000000  CACHE INTERNAL "")
         #SET(LN_MCU_SPEED 96000000  CACHE INTERNAL "")
     ENDIF()
 ELSE() # RISCV
-    SET(LN_ARCH "RISCV" CACHE INTERNAL "")
-    SET(LN_MCU  "VF103" CACHE INTERNAL "")
-    SET(LN_MCU_SPEED 108000000  CACHE INTERNAL "")
+    SET(LN_ARCH "RISCV"              CACHE INTERNAL "")
+    SET(LN_MCU  "VF103"              CACHE INTERNAL "")
+    SET(LN_MCU_SPEED 108000000       CACHE INTERNAL "")
+    SET(LN_MCU_RAM_SIZE    32        CACHE INTERNAL "")
+    SET(LN_MCU_FLASH_SIZE  128       CACHE INTERNAL "")
+    SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
     #SET(LN_MCU_SPEED 96000000  CACHE INTERNAL "")
     #SET(LN_MCU_SPEED 72000000  CACHE INTERNAL "")
 ENDIF()
