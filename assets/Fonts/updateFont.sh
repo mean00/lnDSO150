93eddf28ffed51bbf925034f230081d5f081606c
mean <fixounet@free.fr>
2022-01-22 13:58:37 +0100

    update

---
+++ assets/Fonts/updateFont.sh
@@ -0,0 +1,8 @@
+flatconvert -f ttf/Arimo-Regular.ttf -o Arimo_Regular14pt7b.h -s 14  -m arimo_nc.bin
+#flatconvert -f ttf/Arimo-Regular.ttf -o Arimo_Regular14pt7b.h -s 14 -c -m arimo_c.bin
+flatconvert -f ttf/OpenSans-Regular.ttf -o OpenSans_Regular12pt7b.h  -s 12 -m op12_nc.bin 
+#flatconvert -f ttf/OpenSans-Regular.ttf -o OpenSans_Regular12pt7b.h  -s 12 -c -m op12_c.bin
+flatconvert -f ttf/OpenSans-Regular.ttf -o OpenSans_Regular14pt7b.h  -s 14 
+flatconvert -f ttf/OpenSans-Regular.ttf -o OpenSans_Regular20pt7b.h  -s 20 
+flatconvert -f ttf/Waree.ttf            -o waree9.h -s 9 -m waree_nic.bin
+#flatconvert -f ttf/Waree.ttf            -o waree9.h -s 9 -c -m waree_c.bin
