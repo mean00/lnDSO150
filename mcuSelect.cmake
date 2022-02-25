71177b5749d7d1b88c8f54bd8295187887a1d727
mean <fixounet@free.fr>
2022-02-25 08:26:20 +0100

    disable USB on small ram system too

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -1,18 +1,18 @@
 # We ony enable usb for arm and non small footprint
 IF(NOT DEFINED LN_ARCH)
 IF(1) # ARM
-    IF(0)
+    IF(1)
         SET(LN_ARCH            "ARM" CACHE INTERNAL "")
         SET(LN_MCU             "M3"  CACHE INTERNAL "")
         SET(LN_MCU_RAM_SIZE    20    CACHE INTERNAL "")
-        SET(LN_MCU_STATIC_RAM  4     CACHE INTERNAL "")
+        SET(LN_MCU_STATIC_RAM  6     CACHE INTERNAL "")
         IF(USE_SMALLFOOTPRINT)
           SET(LN_MCU_FLASH_SIZE  64   CACHE INTERNAL "")          
           SET(LN_MCU_EEPROM_SIZE 2     CACHE INTERNAL "")          
         ELSE()
           SET(LN_MCU_FLASH_SIZE  128    CACHE INTERNAL "")
           SET(LN_MCU_EEPROM_SIZE 4     CACHE INTERNAL "")                             
-          SET(LN_ENABLE_USBD True CACHE INTERNAL "")
+          #        SET(LN_ENABLE_USBD True CACHE INTERNAL "")
 
         ENDIF()
         SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
