0c9ac4a04120341632a3c86a0a0f3b3ffddc5234
mean <fixounet@free.fr>
2022-05-08 11:21:49 +0200

    lower the speed to 96M to have both USB and Serial working

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -7,7 +7,7 @@ IF(1) # ARM
       SET(LN_MCU_FLASH_SIZE  256   CACHE INTERNAL "")
       SET(LN_MCU_RAM_SIZE    46    CACHE INTERNAL "")
       SET(LN_MCU_STATIC_RAM  5     CACHE INTERNAL "")
-            SET(LN_MCU_SPEED 108000000    CACHE INTERNAL "") #=> ok
+            SET(LN_MCU_SPEED 96000000    CACHE INTERNAL "") #=> ok
             #      SET(LN_MCU_SPEED  72000000    CACHE INTERNAL "") #=> ok
       SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
       SET(LN_ENABLE_USBD True CACHE INTERNAL "")
