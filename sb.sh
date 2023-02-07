9ffb8aaf33437e9c8b710b8f06e1facf6c88c71e
mean <fixounet@free.fr>
2023-02-07 20:03:47 +0100

    sync lnArduino + update sb.sh for arm use

--- sb.sh
+++ sb.sh
@@ -1,9 +1,9 @@
-export PPP=/arm/prebuilt1502/bin
-export CCC=$PPP/clang-15
-export CPP=$PPP/clang++-15
+export PPP=/arm/tools_llvm/bin
+export CCC=$PPP/clang-17
+export CPP=$PPP/clang++-17
 export ANA=$CCC
-
+set -x
 rm -Rf ../build/*
-/riscv/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi cmake  -DUSE_SCAN_BUILD=True  ..
-/riscv/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi make VERBOSE=1  #-unknown-elf make
+/arm/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi cmake  -DUSE_SCAN_BUILD=True  ..
+/arm/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi make VERBOSE=1  #-unknown-elf make
 
