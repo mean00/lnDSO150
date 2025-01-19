d32cec3d17081017d3fa9cfafcc86a2f1e08767b
mean <fixounet@free.fr>
2025-01-19 14:34:51 +0100

    update lnArduino

--- sb.sh
+++ sb.sh
@@ -2,8 +2,10 @@ export PPP=/arm/tools_llvm/bin
 export CCC=$PPP/clang-18
 export CPP=$PPP/clang++-18
 export ANA=$CCC
+#export SB=/arm/tools_llvm/bin/scan-build
+export SB=~/.local/bin/scan-build
 set -x
 rm -Rf ../build/*
-/arm/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi cmake  -DUSE_SCAN_BUILD=True  ..
-/arm/tools_llvm/bin/scan-build -v --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi make VERBOSE=1  #-unknown-elf make
+${SB} -v --override-compiler --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi cmake  -DUSE_SCAN_BUILD=True  ..
+${SB} -v --override-compiler --use-analyzer=$ANA --use-cc=$CCC --use-c++=$CPP  --analyzer-target=arm-none-eabi make VERBOSE=1  #-unknown-elf make
 
